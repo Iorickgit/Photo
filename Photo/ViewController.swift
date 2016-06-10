480efe00aea598ffad79ee5dd2439df59d56685d
Iori Minami <iori.minami@gmail.com>
2016-06-11 01:45:51 +0900

    Initial Commit

---
+++ Photo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo
+//
+//  Created by 南伊織 on 6/11/16.
+//  Copyright © 2016 南伊織. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
